013cd143f24c010b5af23a047ab28f62638aac20
haeuuu <srhuu15@naver.com>
2021-10-02 21:27:27 +0900

    add : utils to calculate degree-normalized adj matrix

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import scipy.sparse as sp
 from torch.nn import functional as F
 
 def identity_mapping(x):
@@ -14,6 +15,48 @@ activation_map = {
     'none' : identity_mapping
 }
 
+def sparse_to_torch(sp):
+    coo = sp.tocoo()
+    values = torch.FloatTensor(coo.data)
+    indices = torch.LongTensor(np.vstack((coo.row, coo.col)))
+    shape = coo.shape
+
+    return torch.sparse.FloatTensor(indices, values, torch.Size(shape))
+
+def get_degree_inv(graph, edge_types, reverse):
+    if reverse:
+        degree = sum(graph.in_degrees(etype = str(etype)) for etype in edge_types)
+    else:
+        degree = sum(graph.out_degrees(etype = str(etype)) for etype in edge_types)
+
+    degree_inv = torch.pow(degree, -0.5)
+    degree_inv[torch.isinf(degree_inv)] = 0.
+    D_inv = sp.diags(degree_inv.numpy())
+
+    return sparse_to_torch(D_inv)
+
+def get_adjacency(graph, edge_types, ukey = 'user', ikey = 'item'):
+    n_users, n_items = graph.num_nodes(ukey), graph.num_nodes(ikey)
+
+    adj_uv = torch.sparse_coo_tensor(size = (n_users, n_items))
+    for etype in edge_types:
+        adj_uv += graph.adjacency_matrix(etype = str(etype))
+
+    return adj_uv
+
+def degree_noramlization(graph, edge_types, ukey = 'user', ikey = 'item'):
+    adj_uv = get_adjacency(graph, edge_types, ukey, ikey)
+    degree_u = get_degree_inv(graph, edge_types, False)
+    degree_v = get_degree_inv(graph, edge_types, True)
+
+    normed_adj_u = torch.sparse.mm(degree_u, adj_uv)
+    normed_adj_u = torch.sparse.mm(normed_adj_u, degree_v)
+
+    normed_adj_v = torch.sparse.mm(degree_v, adj_uv.t())
+    normed_adj_v = torch.sparse.mm(normed_adj_v, degree_u)
+
+    return normed_adj_u, normed_adj_v
+
 def add_degree(graph, edge_types, symmetric = True, n_users = None, n_items = None):
     def _calc_norm(x):
         x = x.numpy().astype('float32')
@@ -24,25 +67,25 @@ def add_degree(graph, edge_types, symmetric = True, n_users = None, n_items = No
 
     user_ci = []
     user_cj = []
-    movie_ci = []
-    movie_cj = []
+    item_ci = []
+    item_cj = []
     for r in edge_types:
         user_ci.append(graph[f'reverse-{r}'].in_degrees())
-        movie_ci.append(graph[f'{r}'].in_degrees())
+        item_ci.append(graph[f'{r}'].in_degrees())
         
         if symmetric:
             user_cj.append(graph[f'{r}'].out_degrees())
-            movie_cj.append(graph[f'reverse-{r}'].out_degrees())
+            item_cj.append(graph[f'reverse-{r}'].out_degrees())
 
     user_ci = _calc_norm(sum(user_ci))
-    movie_ci = _calc_norm(sum(movie_ci))
+    item_ci = _calc_norm(sum(item_ci))
 
     if symmetric:
         user_cj = _calc_norm(sum(user_cj))
-        movie_cj = _calc_norm(sum(movie_cj))
+        item_cj = _calc_norm(sum(item_cj))
     else:
         user_cj = torch.ones((n_users,))
-        movie_cj = torch.ones((n_items,))
+        item_cj = torch.ones((n_items,))
 
     graph.nodes['user'].data.update({'ci': user_ci, 'cj': user_cj})
-    graph.nodes['item'].data.update({'ci': movie_ci, 'cj': movie_cj})
+    graph.nodes['item'].data.update({'ci': item_ci, 'cj': item_cj})
